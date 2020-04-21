066adb0d7e3427c8cbe75329c25a3d1d515428d8
Cyf <cyf1589247013@gmail.com>
2020-04-21 16:54:30 +0800

    first commit
    Signed-off-by: Cyf <cyf1589247013@gmail.com>

---
+++ mall-ware/src/test/java/com/cyf/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyf.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
